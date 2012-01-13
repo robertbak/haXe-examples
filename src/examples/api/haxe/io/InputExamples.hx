c9d05673c9e0f14aba06e9d329dd2cd29fc29377
robertbak <flex@robertbak.com>
2012-01-13 02:16:25 +0100

    Workign with Input

--- src/examples/api/haxe/io/InputExamples.hx
+++ src/examples/api/haxe/io/InputExamples.hx
@@ -2,16 +2,15 @@ package examples.api.haxe.io;
 
 
 // Input is an abstract class so we need to 
-// use and implementation in the examples
+// use an implementation in the examples
 class InputExamples {
 	public function new(){
 	}
 
     public function readExample(){
        var input = new haxe.io.StringInput("testString"); 
-       var read = input.read(2);
+       var read = input.read(4);
        trace(read);
        trace(Type.getClassName(Type.getClass(read)));
-       
     }
 }
