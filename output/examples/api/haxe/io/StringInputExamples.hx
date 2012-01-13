abedc5073ce2922af603a2e829d6714d7cd91a64
robertbak <flex@robertbak.com>
2012-01-13 01:18:37 +0100

    StringInput example

--- output/examples/api/haxe/io/StringInputExamples.hx
+++ output/examples/api/haxe/io/StringInputExamples.hx
@@ -1,8 +1,22 @@
 package examples.api.haxe.io;
-// No traces
+
+import haxe.io.StringInput;
+import haxe.io.BytesInput;
 
 class StringInputExamples {
 	public function new(){
 	}
 
+    public function sample_use(){
+        // You can't initalize a BytesInput with a string
+        // eg. var bytesInput = new BytesInput("testString");
+        // will throw a compile time error
+        
+        // To make the initialization easier StringInput extends BytesInput 
+        var stringInput = new StringInput("testString"); 
+        trace(stringInput.readString(4));
+        // test
+
+
+    }
 }
--- src/examples/api/haxe/io/StringInputExamples.hx
+++ src/examples/api/haxe/io/StringInputExamples.hx
@@ -1,7 +1,21 @@
 package examples.api.haxe.io;
 
+import haxe.io.StringInput;
+import haxe.io.BytesInput;
+
 class StringInputExamples {
 	public function new(){
 	}
 
+    public function sample_use(){
+        // You can't initalize a BytesInput with a string
+        // eg. var bytesInput = new BytesInput("testString");
+        // will throw a compile time error
+        
+        // To make the initialization easier StringInput extends BytesInput 
+        var stringInput = new StringInput("testString"); 
+        trace(stringInput.readString(4));
+
+
+    }
 }
